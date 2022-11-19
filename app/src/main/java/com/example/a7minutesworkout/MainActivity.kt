35db6a8df3eb0961183c7cecf9576e6b73c46040
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2022-11-19 13:14:47 +0530

    create startup page UI

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStart: FrameLayout = findViewById(R.id.flStart)
+        flStart.setOnClickListener {
+            Toast.makeText(this@MainActivity,
+                "Button Clicked Successfully", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
